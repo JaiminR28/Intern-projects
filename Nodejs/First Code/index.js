adb09cd7c7a6eeb94ce00552fc0b22f1f71f31d5
JaiminR28 <jaiminrathwa28@gmail.com>
2024-01-22 19:46:05 +0530

    Express package added

--- Nodejs/First Code/index.js
+++ Nodejs/First Code/index.js
@@ -1,47 +1,18 @@
 const http = require("http");
-const fs = require("fs");
+const routes = require("./routes");
+const express = require("express");
+
+const app = express();
+
+app.use((req, res, next) => {
+	console.log("This is a middleware");
+	next();
+});
 
 // console.log("Hello World");
 
-const server = http.createServer((req, res) => {
-	// console.log(req);
-	const url = req.url;
-	const method = req.method;
+const server = http.createServer(app);
 
-	if (url == "/") {
-		res.setHeader("Content-Type", "text/html");
-		res.write("<html>");
-		res.write("<head><title>Enter Message</title></head>");
-		res.write(
-			"<body><form action='/message' method='POST'><input type='text'name='message'> <button >Click here !!</button> </form></body>"
-		);
-		res.write("</html>");
-		return res.end();
-	}
-	if (url === "/message" && method === "POST") {
-		const body = [];
-		req.on("data", (chunk) => {
-			body.push(chunk);
-		});
-		req.on("end", () => {
-			const parsedBody = Buffer.concat(body).toString();
-			const message = parsedBody.split("=")[1];
-			fs.writeFileSync("message.txt", message);
-			//! This is an async task
-			res.statusCode = 302;
-			res.setHeader("Location", "/");
-			return res.end();
-		});
-	}
-
-	res.setHeader("Content-Type", "text/html");
-	res.write("<html>");
-	res.write("<head><title>This is the title</title></head>");
-	res.write("<body>You have been redirected</body>");
-	res.write("</html>");
-	res.end();
-});
-
-server.listen(8000, () => {
+server.listen(3000, () => {
 	"server is listeining";
 });
