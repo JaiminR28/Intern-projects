add2662a563030b94b31c022debd3e2fe69e34cd
JaiminR28 <jaiminrathwa28@gmail.com>
2024-01-23 11:02:11 +0530

    Moved all the react-redux project files in to the seperate folder

--- Nodejs/First Code/index.js
+++ Nodejs/First Code/index.js
@@ -4,9 +4,21 @@ const express = require("express");
 
 const app = express();
 
-app.use((req, res, next) => {
+app.use("/", (req, res, next) => {
 	console.log("This is a middleware");
 	next();
+});
+
+app.use("/add-product", (req, res, next) => {
+	console.log("Into the another middleware");
+	res.send(
+		"<form> <input type='text' name='title' /> <button>Click here</button> </form>"
+	);
+});
+
+app.use("/", (req, res, next) => {
+	console.log("Into the last middleware");
+	res.send("<h1>Hello from the Express !!</h1>");
 });
 
 // console.log("Hello World");
